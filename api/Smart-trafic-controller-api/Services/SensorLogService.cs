9290cfa644a6b765947d814b499e58a9a5e5ce54
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2025-11-04 20:27:36 +0100

    Removed additional try-catches that i had overlooked

--- api/Smart-trafic-controller-api/Services/SensorLogService.cs
+++ api/Smart-trafic-controller-api/Services/SensorLogService.cs
@@ -12,20 +12,14 @@ namespace Smart_traffic_controller_api.Services
 
         public async Task<List<SensorLogResponseDTO>> GetAllSensorLogsAsync()
         {
-            try
-            {
-                List<SensorLog> sensorLogs = await _sensorLogRepository.GetAllSensorLogsAsync();
+            List<SensorLog> sensorLogs = await _sensorLogRepository.GetAllSensorLogsAsync();
 
-                if (sensorLogs == null || sensorLogs.Count == 0)
-                {
-                    return new List<SensorLogResponseDTO>();
-                }
-                return SensorLogMapper.ToResponseListDTO(sensorLogs);
-            }
-            catch (Exception ex)
+            if (sensorLogs == null || sensorLogs.Count == 0)
             {
-                throw new Exception("An error occurred while retrieving all sensor logs.", ex);
+                return new List<SensorLogResponseDTO>();
             }
+
+            return SensorLogMapper.ToResponseListDTO(sensorLogs);
         }
 
         public async Task<List<SensorLogResponseDTO>> GetSensorLogsByTimeRangeAsync(
@@ -33,55 +27,29 @@ namespace Smart_traffic_controller_api.Services
             DateTime endTime
         )
         {
-            try
-            {
-                List<SensorLog> sensorLogs =
-                    await _sensorLogRepository.GetSensorLogsByTimeRangeAsync(startTime, endTime);
+            List<SensorLog> sensorLogs = await _sensorLogRepository.GetSensorLogsByTimeRangeAsync(
+                startTime,
+                endTime
+            );
 
-                if (sensorLogs == null || sensorLogs.Count == 0)
-                {
-                    return new List<SensorLogResponseDTO>();
-                }
-
-                return SensorLogMapper.ToResponseListDTO(sensorLogs);
-            }
-            catch (Exception ex)
+            if (sensorLogs == null || sensorLogs.Count == 0)
             {
-                throw new Exception(
-                    "An error occurred while retrieving sensor logs by time range.",
-                    ex
-                );
+                return new List<SensorLogResponseDTO>();
             }
+
+            return SensorLogMapper.ToResponseListDTO(sensorLogs);
         }
 
         public async Task CreateSensorLogAsync(SensorValue sensorValue)
         {
-            try
+            if (sensorValue.GetType() != typeof(SensorValue))
             {
-                if (sensorValue.GetType() != typeof(SensorValue))
-                {
-                    throw new ArgumentException(
-                        "Sensor type and sensor value cannot be null or empty."
-                    );
-                }
-
-                SensorLog createdSensorLog = await _sensorLogRepository.CreateSensorLogAsync(
-                    new SensorLog(sensorValue)
+                throw new ArgumentException(
+                    "Sensor type and sensor value cannot be null or empty."
                 );
-                return;
             }
-            catch (ArgumentNullException argEx)
-            {
-                throw new ArgumentNullException(argEx.Message, argEx);
-            }
-            catch (ArgumentException argEx)
-            {
-                throw new ArgumentException(argEx.Message, argEx);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("An error occurred while creating the sensor log.", ex);
-            }
+
+            await _sensorLogRepository.CreateSensorLogAsync(new SensorLog(sensorValue));
         }
     }
 }
